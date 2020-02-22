5973cdc44bcbae87ef26334b080cf02505ce1f15
popiette <pierre.jacquot@insa-lyon.fr>
2020-02-22 15:47:20 +0100

    Launch analysis.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
-#include "lexer.h"
 
+#include "lexer.h"
+#include "Automate.h"
 
 int main(void) {
    string chaine("(1+34)*123");
-
+/*
    Lexer l(chaine);
 
    Symbole * s;
@@ -13,6 +14,9 @@ int main(void) {
       cout<<endl;
       l.Avancer();
    }
+*/
+   Automate a(chaine);
+   a.analyse();
    return 0;
 }
 
